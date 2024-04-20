f32db5d7c064f27d941c51a604818745be94c0aa
Om kharade <146190558+Omkharade318@users.noreply.github.com>
2024-04-20 22:25:29 +0530

    Add files via upload

---
+++ Functions.kt
@@ -0,0 +1,11 @@
+fun main(){
+   println(sum(4,5))
+   println(subtract(4,5))
+}
+
+fun sum(num1 : Int, num2 : Int) : Int  // normal fuction
+{
+    return num1 + num2
+}
+
+fun subtract(num1 : Int, num2 : Int) = num1 - num2   // Inline fuction
