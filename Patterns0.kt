05306e887cbbd81d9dfdbf64b2fbfa4ad53d401b
Om kharade <146190558+Omkharade318@users.noreply.github.com>
2024-05-19 20:26:48 +0530

    Add files via upload

---
+++ Patterns0.kt
@@ -0,0 +1,21 @@
+fun main(){
+
+    print("Enter a number: ")
+    val n = readLine()!!.toInt()
+
+    for (i in 1 .. n){
+
+        for (j in 1 .. n)
+        print("$")
+
+    println()    
+    }
+}
+
+//  $$$$    for input 4
+//  $$$$   
+//  $$$$
+//  $$$$
+
+
+
